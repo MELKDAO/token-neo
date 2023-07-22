2d46f25da51d560a79ff7c2bc27cd878f7a8f872
Lorenzobattistela <lorenzobattistela@gmail.com>
2023-07-22 12:06:47 -0300

    [feat] creating deploy and interacting with neo3 contrcat script

---
+++ scripts/deploy.py
@@ -0,0 +1,81 @@
+from pathlib import Path
+from neo3.core import types
+import asyncio
+from neo3.api.wrappers import (
+    GenericContract,
+    ChainFacade,
+    NeoToken,
+    GasToken,
+    NEP17Contract,
+    ContractMethodResult,
+)
+from neo3.api.helpers.signing import sign_insecure_with_account
+from neo3.api.helpers import unwrap
+from neo3.contracts import nef, manifest
+from neo3.network.payloads.verification import Signer
+from neo3.wallet.account import Account
+from neo3.api.noderpc import NeoRpcClient
+
+private_key = ""  # private key of the account that will deploy the contract
+password = ""  # password of the account that will deploy the contract
+
+account = Account.from_wif(private_key, password)
+
+
+async def deploy(facade: ChainFacade):
+    root = Path(__file__).parent.parent
+    nef_file = root / "contracts" / "melk.nef"
+    nef_contract = nef.NEF.from_file(nef_file)
+
+    manifest_file = root / "contracts" / "melk.manifest.json"
+    manifest_contract = manifest.ContractManifest.from_file(manifest_file)
+
+    print("Deploying contract ...")
+    receipt = await facade.invoke(
+        GenericContract.deploy(nef_contract, manifest_contract)
+    )
+    print("done")
+    return receipt
+
+
+async def main():
+    facade = ChainFacade.node_provider_testnet()
+    facade.rpc_host = "http://seed1t5.neo.org:20332"  # fixing wrong RPC TESTNET
+
+    facade.add_signer(
+        sign_insecure_with_account(account, password=password),
+        Signer(account.script_hash),
+    )
+
+    # Uncomment this if you want to deploy a new contract. Then, replace the contract hash below in the string
+
+    # receipt = await deploy(facade=facade)
+    # print(receipt)
+
+    contract_hash = types.UInt160.from_string(
+        "0x22a0b0488be9daf510c0a9e656016e3302dd38a1"
+    )
+
+    token = NEP17Contract(contract_hash)
+
+    balance = await facade.test_invoke(token.balance_of(account.address))
+    print(f"Current MELK token balance: {balance}")
+
+    total_supply = await facade.test_invoke(token.total_supply())
+    print(f"Total supply: {total_supply.stack[0].value}")
+
+    is_paused = await facade.test_invoke(token.call_function("is_paused"))
+    print(f"Is paused: {is_paused.stack[0].value}")
+
+    destination = "NaQSq2VexRSoWgKHmbM8qVbLPpZqefD5SL"  # replace this with the destination address
+    print("Transfering MELK tokens")
+    res = await facade.invoke(token.transfer(account.address, destination, 10))
+    print(res)
+
+    print("Checking if balance changed")
+    balance = await facade.test_invoke(token.balance_of(destination))
+    print(balance)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
